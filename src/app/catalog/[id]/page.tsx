0e0091f7f47e4c96cc1ff02d279b42deb50b6a9a
Kennynna <kennynna@mail.ru>
2025-05-15 22:39:45 +0300

    fix

--- src/app/catalog/[id]/page.tsx
+++ src/app/catalog/[id]/page.tsx
@@ -1,8 +1,14 @@
 import { ItemInfo } from '@/components/widgets/item-info'
 import { ItemSwiper } from '@/components/widgets/item-swiper'
 
-export default function ProductPage({ params }: { params: { id: string } }) {
-	const { id } = params
+
+type ProductPageProps = {
+	params: { id: string }
+	searchParams?: { [key: string]: string | string[] | undefined }
+}
+
+export default function ProductPage({ params }: ProductPageProps) {
+	const { id } = params as { id: string }
 	return (
 		<div className='p-4 mx-auto my-0 w-max-[1260px]'>
 			<ItemSwiper />
@@ -12,7 +18,6 @@ export default function ProductPage({ params }: { params: { id: string } }) {
 				<button className='px-[50px] py-[16px] bg-[#DFD5D5] text-black min-w-[280px] font-bold'>
 					Оформить
 				</button>
-
 			</div>
 		</div>
 	)
