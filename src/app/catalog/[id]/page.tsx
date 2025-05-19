d1a5e209e86cbeeb9482d5e64dc1b15a582e57dd
Kennynna <kennynna@mail.ru>
2025-05-19 17:20:02 +0300

    add new

--- src/app/catalog/[id]/page.tsx
+++ src/app/catalog/[id]/page.tsx
@@ -1,19 +1,21 @@
-import { ItemInfo } from "@/components/widgets/item-info"
-import { ItemSwiper } from "@/components/widgets/item-swiper"
+import { ItemInfo } from '@/components/widgets/item-info'
+import { ItemSwiper } from '@/components/widgets/item-swiper'
 
 type ProductPageProps = {
-	params: { id: string }
-	searchParams?: { [key: string]: string | string[] | undefined }
+	params: Promise<{ id: string }>
+	searchParams?: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
-// Серверный компонент — может быть async, если вы делаете fetch внутри
-export default async function ProductPage({ params }: ProductPageProps) {
-	const { id } = params
+export default async function ProductPage(props: ProductPageProps) {
+	// await the params/searchParams before using
+	const { id } = await props.params
+	// if you need searchParams:
+	// const sp = await props.searchParams
 
 	return (
 		<div className='p-4 mx-auto my-0 max-w-[1260px]'>
-			<ItemSwiper /* product={product} */ />
-			<ItemInfo /* product={product} */ />
+			<ItemSwiper /* product={...} */ />
+			<ItemInfo /* product={...} */ />
 			<div className='flex flex-col gap-6 mt-10'>
 				<button className='px-[50px] py-[16px] bg-[#DFD5D5] text-black min-w-[280px] font-bold'>
 					Оформить
